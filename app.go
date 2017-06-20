995cbf4c1d895276d013f3471b2e593a3ccbf7ec
user <user@host>
2017-06-21 00:54:50 +0200

    Further breaking up the corsiar package into smaller pieces that can be independently implemented, and require less total memory by removing global variables and packaging them in structs that can be added to the main application instance struct

--- app.go
+++ app.go
@@ -9,12 +9,14 @@ import (
 	cli "gopkg.in/alecthomas/kingpin.v2"
 	// ## Corsair Framework ##
 	"corsair/config"
+  "corsair/config/logging"
 	"corsair/config/version"
 	"corsair/models"
+  "corsair/lifecycle/sigtrap"
 	"corsair/database/memory"
 	"corsair/corsair"
 	_ "corsair/network/http"
-	"corsair/network/tls"
+	tls "corsair/network/tls"
 	// ## *User Defined Plugins* ##
 	// ## Include plugins here, with the special
 	// ## character '_' ifront of the imported library.
@@ -22,13 +24,13 @@ import (
 	_ "plugins/corsair-search"
 )
 
-type Daemon struct {
-  // Using embedding principle to inherit the corsair application object
-  App       corsair.Application
+type Application struct {
+  Name          string
+  Instance      corsair.Instance
 }
 
-func (self *Process) flagsAndCommands(){
-  args           := cli.New("corsair", "Corsair application framework")
+func (self *Application) flagsAndCommands(){
+  args           :=  cli.New("corsair", "Corsair application framework")
   quiet          :=  args.Flag("quiet", "List installed plugins, not necessarily actived plugins.").Bool()
   configPath     :=  args.Flag("config", "Corsairfile to load, default location is the current working directory.").String()
   maxCPU         :=  args.Flag("cpu", "Maximum CPU allowed for the Corsair application process.").Default("100%").String()
@@ -42,7 +44,7 @@ func (self *Process) flagsAndCommands(){
   pluginItem     :=  plugin.Arg("item", "Available actions include: list, update, install.")
   revoke         :=  args.Command("revoke", "Revoke a certificate associated with specified domain.")
   revokeHost     :=  revoke.Arg("host", "All certiciates with this hostname will be removed.")
-  validate       := args.Command("validate", "Parse the Corsairfile without starting the server")
+  validate       :=  args.Command("validate", "Parse the Corsairfile without starting the server")
   version        :=  args.Command("version", "Show version and other build information of the application.")
   versionFlag    :=  args.Flag("version", "Show version and other build information of the application.").Bool()
   if *versionFlag {
@@ -50,24 +52,24 @@ func (self *Process) flagsAndCommands(){
     os.Exit(0)
   }
   if *configPath != "" {
-    self.App.ConfigPath = *configPath
+    self.Instance.ConfigPath = *configPath
   }
   if *logPath != "" {
-    self.App.Config.LogPath = *logPath
+    self.Instance.Config.LogPath = *logPath
   }
   if *pidPath != "" {
-    self.App.Config.PIDPath = *pidPath
+    self.Instance.Config.PIDPath = *pidPath
   }
   if *maxCPU != "" {
-    self.App.Config.MaxCPU = *maxCPU
+    self.Instance.Config.MaxCPU = *maxCPU
   }
   if *quiet {
-    self.App.Config.Quiet = *quiet
+    self.Instance.Config.Quiet = *quiet
   }
 
   switch cli.MustParse(args.Parse(os.Args[1:])) {
   case plugins.FullCommand():
-		fmt.Println(corsair.DescribePlugins())
+		fmt.Println(self.Instance.DescribePlugins())
 		os.Exit(0)
   case plugin.FullCommand():
     fmt.Println("[Error] Currently unimplemented plugin")
@@ -75,7 +77,7 @@ func (self *Process) flagsAndCommands(){
     fmt.Println("CAN YOU USE ", revokeHost, " instead of os.Args[2]?")
 		err := corsairtls.Revoke(os.Args[2])
 		if err != nil {
-			mustLogFatalf("%v", err)
+			logging.MustLogFatalf("%v", err)
 		}
 		fmt.Printf("Revoked certificate for %s\n", os.Args[2])
 		os.Exit(0)
@@ -83,60 +85,61 @@ func (self *Process) flagsAndCommands(){
     self.printBanner()
     os.Exit(0)
   case validate.FullCommand():
-		err := corsair.ValidateAndExecuteDirectives(self.Input, nil, true)
+		err := self.Instance.ValidateAndExecuteDirectives(self.Instance.Input, nil, true)
 		if err != nil {
-			mustLogFatalf("%v", err)
+			logging.MustLogFatalf("%v", err)
 		}
 		msg := "Corsairfile is valid"
 		fmt.Println(msg)
+    // TODO: Create different log types in logging, so you can just do
+    // logging.info("msg") and have it colored appropriately.
 		log.Printf("[INFO] %s", msg)
 		os.Exit(0)
   }
 }
 
-func (self *Daemon)printBanner() {
+func (self *Application)printBanner() {
   fmt.Println("  _____                    ")
   fmt.Println(" /     \\                   ")
   fmt.Println("| () () __________________ ")
   fmt.Println(" \\  ^  /                  |")
-  fmt.Println("  | |", self.Name, "/",)
+  fmt.Println("  | |", self.App.Name, "/",)
   fmt.Println("  |||||___________________|")
-  fmt.Println("              ", self.Build.Version.ToString(), "")
+  fmt.Println("              ", self.App.Build.Version.ToString(), "")
 }
 
 func main() {
 	//corsair.RegisterCorsairfileLoader("flag", corsair.LoaderFunc(confLoader))
 	//corsair.SetDefaultCorsairfileLoader("default", corsair.LoaderFunc(defaultLoader))
   workingPath, _ := os.Executable()
-  daemon = Daemon{
-    App:   corsair.Application{
-      Name:             "Corsair",
+  app := Application{
+    Name:             "Corsair",
+    Instance:   corsair.Instance{
       WorkingPath:      workingPath,
       Config:           config.Load(workingPath+"corsair.yaml"),
       MemoryDB:         memory.Open(":memory:"),
+      ConfigFilename:   "Corsairfile",
       TLS:  models.TLS{
         CAUrl: "https://acme-v01.api.letsencrypt.org/directory",
         Email: "Default ACME CA account email address",
       },
       Build: models.Build{
         Version: version.Version{Major: 0, Minor: 1, Patch: 0},
-	    Development: true,
+	      Development: true,
       },
     },
   }
-	daemon.App.TrapSignals()
-  daemon.printBanner()
-	daemon.flagsAndCommands()
-	acme.UserAgent = daemon.App.Config.Server.UserAgent
-
-	corsair.EmitEvent(corsair.StartupEvent, nil)
-	daemon.App.Input = config.LoadCorsairfile(daemon.App.Config.Server.ServerType)
-
-	instance, err := corsair.Start(corsairfileinput)
+	app.Instance.TrapSignals()
+  app.printBanner()
+	app.flagsAndCommands()
+  // TODO: This is weird, there must be a better way
+	acme.UserAgent = app.Instance.Config.Server.UserAgent
+	app.Instance.EmitEvent(app.Instance.StartupEvent, nil)
+	app.Instance.Input = app.Instance.LoadCorsairfile(app.Instance.Config.Server.ServerType)
+	app, err := app.Instance.Start(corsairfileinput)
 	if err != nil {
-		mustLogFatalf("%v", err)
+		logging.mustLogFatalf("%v", err)
 	}
-
-	instance.Wait()
+	app.Instance.Wait()
 }
 
